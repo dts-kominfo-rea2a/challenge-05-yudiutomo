7b273a0a8dfbcb2e9b0a0a7763aaa17fbc955cc1
Yudi Utomo <acropolix@gmail.com>
2022-06-05 03:40:24 +0800

    Finish challenge 05

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (args, callback) => {
+  const dataSortir = [];
+  let counter = 0;
+  callback(args);
+  for (let data of args) {
+    ++counter;
+    dataSortir.push(`${counter}. ${data}`);
+  }
+  return dataSortir;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (args) => {
+  args.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (args) => {
+  args.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
